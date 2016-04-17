e5059793ba3905dd0726e2754f929427c18debc4
Brian Farnsworth <farnswor@adobe.com>
2016-04-17 13:27:37 -0600

    Got basic read and write to core data on phone

--- Level Up/ViewController.swift
+++ Level Up/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var qualities = [NSManagedObject]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+          print(qualities)
+        
+//        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
+//        let managedContext = appDelegate.managedObjectContext;
+//        let entity = NSEntityDescription.entityForName("Quality", inManagedObjectContext:managedContext);
+//        let quality = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext);
+//        
+//        quality.setValue("Happiness", forKey: "name");
+//        quality.setValue(0, forKey: "count");
+        
+//        do{
+//            try managedContext.save();
+//            qualities.append(quality);
+//        } catch let error as NSError {
+//            print("Could not save \(error), \(error.userInfo)")
+//        }
+        
+        
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
+        let managedContext = appDelegate.managedObjectContext;
+        let fetchRequest = NSFetchRequest(entityName: "Quality");
+        
+        do {
+            let results =
+                try managedContext.executeFetchRequest(fetchRequest)
+            qualities = results as! [NSManagedObject]
+            print("qualities \(qualities.count)")
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +61,7 @@ class ViewController: UIViewController {
     }
     
     var count = 0;
+
     
     @IBOutlet weak var happinessCountLabel: UILabel!
     @IBAction func didClickHappinessButton(sender: UIButton) {
