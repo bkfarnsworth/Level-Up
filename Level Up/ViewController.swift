b48a8fe1ef61fe5eb48e32540c4b9ee9da061862
Brian Farnsworth <farnswor@adobe.com>
2016-04-17 12:07:46 -0600

    Got a blank project running on my iPhone and apple watch

---
+++ Level Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Level Up
+//
+//  Created by Brian Farnsworth on 4/17/16.
+//  Copyright © 2016 Brian Farnsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
